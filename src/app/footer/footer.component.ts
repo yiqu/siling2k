a3f9ad0caf573530fec14c4a8b6f6db4872db0aa
Kevin Qu <kevinqu88@gmail.com>
2021-10-10 20:31:50 -0400

    init

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppFooterComponent implements OnInit {
 
-  footerTitle: string = "@KQ 2020";
+  footerTitle: string = "@KQ 2021";
   myUrl: string = "https://yiqu.github.io/";
 
 
