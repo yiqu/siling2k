85ab920a99fd5e302129f3d3280bfe1ba2bfcee6
Kevin Qu <kevinqu88@gmail.com>
2021-10-29 22:57:18 -0400

    drag and drop init

---
+++ src/app/settings/core/show-hide-drag/show-hide-drag.component.ts
@@ -0,0 +1,41 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-core-show-hide',
+  templateUrl: 'show-hide-drag.component.html',
+  styleUrls: ['./show-hide-drag.component.scss']
+})
+export class ShowHideComponent implements OnInit {
+
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event)
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+}
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SharedBudleModule } from '../shared/shared.module';
 import { SettingsCoreComponent } from './core/core.component';
+import { ShowHideComponent } from './core/show-hide-drag/show-hide-drag.component';
 import { SettingsRoutingModule } from './settings-routing.module';
 
 import { SettingsComponent } from './settings.component';
@@ -15,7 +16,8 @@ import { SettingsComponent } from './settings.component';
 
   declarations: [
     SettingsComponent,
-    SettingsCoreComponent
+    SettingsCoreComponent,
+    ShowHideComponent
   ],
 
   providers: [],
