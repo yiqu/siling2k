ee1ffd078114e000c795f4bf12918f8a44d1a122
Kevin Qu <kevinqu88@gmail.com>
2021-11-08 23:01:41 -0500

    finished resolver for getting shown silings

--- src/app/core/core.service.ts
+++ src/app/core/core.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { SilingCompany } from '../admin/store/admin.state';
 import { SilingData } from '../models/general.models';
+import { ShowHideCompanyList, ShowHideList } from '../settings/store/settings.state';
+import { RestService } from '../shared/services/rest.service';
 import { AppState } from '../store/global/app.reducer';
 import * as fromCoreActions from './store/core.actions';
 import * as fromCoreSelectors from './store/core.selectors';
@@ -14,7 +17,7 @@ export class SilingCoreService {
   public isEntrySaveLoading$: Observable<boolean> = this.store.select(fromCoreSelectors.getSaveIsLoading);
 
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>, private rs: RestService) {
 
   }
 
@@ -26,6 +29,14 @@ export class SilingCoreService {
     if (names && names.length > 0) {
       this.store.dispatch(fromCoreActions.getSilingDataByNameStart({names: names}));
     }
+  }
+
+  public fetchSilingCompanies(): Observable<SilingCompany[]> {
+    return this.rs.getCollection<SilingCompany>('settings/kqpro/companies/all');
+  }
+
+  public fetchShowHideList(): Observable<ShowHideCompanyList> {
+    return this.rs.getDocument('settings/kqpro/showHideList');
   }
 
 }
--- src/app/shared/resolvers/dashboard.resolver.ts
+++ src/app/shared/resolvers/dashboard.resolver.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
 import { switchMap, take, tap } from 'rxjs/operators';
+import { SilingCompany } from 'src/app/admin/store/admin.state';
 import { SilingCoreService } from 'src/app/core/core.service';
 import { SettingsService } from 'src/app/settings/settings.service';
 import { environment } from 'src/environments/environment';
@@ -12,33 +13,54 @@ import { environment } from 'src/environments/environment';
 export class DashboardLoadDataFireResolver implements Resolve<any> {
   constructor(private cs: SilingCoreService, private ss: SettingsService) {}
 
-  // need to call both show hide list, and all company list
+  private getCompaniesBeingDisplayed$ = forkJoin({
+    allCompanies: this.cs.fetchSilingCompanies(),
+    showHideList: this.cs.fetchShowHideList()
+  })
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
-    return this.ss.showHideList$.pipe(
+    return this.getCompaniesBeingDisplayed$.pipe(
       take(1),
       tap((res) => {
-        console.log("resolver for comapany list", res);
-        const companyIds = environment.defaultSilingInsToLoad.map(
-          (res) => {
-            return res.id;
+        const allCompanies: SilingCompany[] = res.allCompanies;
+        const comapnyToShow: SilingCompany[] = [];
+        let companiesHidden: SilingCompany[] = [];
+        let companiesShown: SilingCompany[] = [];
+
+        if (res.showHideList && res.showHideList.hiding && res.showHideList.hiding.length > -1) {
+          companiesHidden = res.showHideList.hiding;
+        }
+
+        if (res.showHideList && res.showHideList.showing && res.showHideList.showing.length > -1) {
+          companiesShown = res.showHideList.showing;
+        }
+
+        allCompanies.forEach((res: SilingCompany) => {
+          const excludeIndex: number = companiesHidden.findIndex((co) => {
+            return co.name === res.name;
+          });
+          if (excludeIndex < 0) {
+            comapnyToShow.push(res);
           }
-        )
-        this.cs.getSilingDataByInsName(companyIds);
+        });
+
+        comapnyToShow.sort((x: SilingCompany, y: SilingCompany) => {
+          const xIndex = companiesShown.findIndex((co) => {
+            return co.id === x.id;
+          });
+          const yIndex = companiesShown.findIndex((co) => {
+            return co.id === y.id;
+          });
+
+          return xIndex > yIndex ? 1 : -1;
+        });
+
+        const companiesToShowIds = comapnyToShow.map((showCo) => {
+          return showCo.name.toLowerCase();
+        });
+
+        this.cs.getSilingDataByInsName(companiesToShowIds);
       })
     );
   }
 }
-
-
-/**
- * Resolver based on route params
- */
-// @Injectable()
-// export class APIResolver implements Resolve<any> {
-//   constructor(private apiService: APIService) {}
-
-//   resolve(route: ActivatedRouteSnapshot) {
-//     return this.apiService.getItems(route.params.date);
-//   }
-// }
