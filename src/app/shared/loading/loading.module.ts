8acc37b79240f99dae035a81053671c1d4e5c7ab
Kevin Qu <kevinqu88@gmail.com>
2021-11-27 22:50:26 -0500

    loading fix

--- src/app/shared/loading/loading.module.ts
+++ src/app/shared/loading/loading.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { LoadingComponent } from './loading.component';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule
   ],
 
   exports: [
